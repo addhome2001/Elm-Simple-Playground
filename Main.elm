c4cc936c0a8919f3d6715bde0e48c6b657e6812e
Seven <seven@megalive.com.tw>
2017-02-20 00:50:18 +0800

    Added prompt text

--- Main.elm
+++ Main.elm
@@ -2,7 +2,8 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput, onClick)
+import Html.Events exposing (on, keyCode, onInput, onClick)
+import Json.Decode as Json
 
 
 main : Program Never Model Msg
@@ -16,6 +17,18 @@ main =
 
 
 -- MODEL
+
+
+onEnter : Msg -> Attribute Msg
+onEnter msg =
+    let
+        isEnter code =
+            if code == 13 then
+                Json.succeed msg
+            else
+                Json.fail "not ENTER"
+    in
+        on "keydown" (Json.andThen isEnter keyCode)
 
 
 type alias Model =
@@ -62,6 +75,7 @@ view model =
         , input
             [ placeholder "Typing here."
             , onInput ChangeMessage
+            , onEnter ClearMessage
             , value model.message
             , maxlength 18
             , class "input-area"
@@ -72,4 +86,7 @@ view model =
             , class "clear-btn"
             ]
             [ text "Clear Message" ]
+        , div
+            [ class "prompt" ]
+            [ text "Or press Enter to clear message" ]
         ]
